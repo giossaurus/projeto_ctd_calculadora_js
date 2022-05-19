9aa42702f6b12bc3237c329338c96db743def3e5
Giovanni Della Dea <91167613+giossaurus@users.noreply.github.com>
2022-05-19 00:21:44 -0300

    CSS atualizado e JS resumido

--- script.js
+++ script.js
@@ -18,9 +18,5 @@ function calcular()
 {
     var resultado = document.getElementById('resultado').innerHTML;
     if(resultado)
-    {
-        document.getElementById('resultado').innerHTML = eval(resultado);
-    }else{
-        document.getElementById('resultado').innerHTML = "";
-    }
+    document.getElementById('resultado').innerHTML = eval(resultado);
 }
